5cd03f2afb633a4357e808015a788dcc50136278
davix1997 <davixville@gmail.com>
2025-04-09 11:45:44 +0200

    svelte

--- laparoscopia/src/App.svelte
+++ laparoscopia/src/App.svelte
@@ -1,36 +1,29 @@
-<div class="prueba">
-    <div class="vertical-1" id="id-instrumento1">
-        <svg width="70" height="70" viewBox="0 0 100 100">
-            <polyline id="instrument-1" points="10,-40 80,50 10,140" stroke="white" stroke-width="3" fill="none"/>
-        </svg>
-        <input type="range" min="0" max="100" disabled value="0" id="depth-1" style="width: 160px;">
-        <p class="number">1</p>
-    </div>    
-    <div class="vertical-4" id="id-instrumento4">
-        <svg width="70" height="70" viewBox="0 0 100 100">
-            <polyline id="instrument-4" points="10,-40 80,50 10,140" stroke="white" stroke-width="3" fill="none"/>
-        </svg>
-        <input type="range" min="0" max="100" value="60" id="depth-4" disabled style="width: 160px;">
-        <p class="number">4</p>
-    </div>   
-    <div class="vertical-2" id="id-instrumento2">
-        <svg width="70" height="70" viewBox="0 0 100 100">
-            <polyline id="instrument-2" points="10,-40 80,50 10,140" stroke="white" stroke-width="3" fill="none"/>
-        </svg>
-        <input type="range" min="0" max="100" disabled value="0" id="depth-2" style="width: 160px;">
-        <p class="number">2</p>
-    </div>   
-    <div class="vertical-3" id="id-instrumento3">
-        <svg width="70" height="70" viewBox="0 0 100 100">
-            <polyline id="instrument-3" points="10,-40 80,50 10,140" stroke="white" stroke-width="3" fill="none"/>
-        </svg>
-        <input type="range" min="0" max="100" disabled value="0" id="depth-3" style="width: 160px;">
-        <p class="number">3</p>
-    </div>  
-</div>
-<div class="camera">
-    <label for="" style="color: white;">&#8722;&nbsp;</label><input type="range" min="0" max="100" value="0" disabled id="camera" style="width: 160px;"><label for="" style="color: white;">&nbsp;&#43;</label>
-</div>
-<div class="messages" style="display: none;">
-    <p>msg de error</p>
-</div>
+<script>
+    import Camera from "./components/Camera.svelte";
+    import Message from "./components/Message.svelte";
+    import Instruments from "./components/Instruments.svelte";
+    import CameraControls from "./components/CameraControls.svelte";
+
+    let depth1 = 0;
+    let depth2 = 0;
+    let depth3 = 0;
+    let depth4 = 0;
+
+    let points1 = "10,-40 80,50 10,140";
+    let points2 = "10,-40 80,50 10,140";
+    let points3 = "10,-40 80,50 10,140";
+    let points4 = "10,-40 80,50 10,140";
+
+
+</script>
+<CameraControls/>
+<Message/>
+<Instruments position = 'top-right' number = 1 bind:depth = {depth1} points = {points1}/>
+<Instruments position = 'top-left' number = 2 bind:depth = {depth2} points = {points2}/>
+<Instruments position = 'bottom-left' number = 3 bind:depth = {depth3} points = {points3}/>
+<Instruments position = 'bottom-right' number = 4 bind:depth = {depth4} points = {points4}/>
+<style>
+    :global(body){
+        background-color: antiquewhite;
+    }
+</style>
