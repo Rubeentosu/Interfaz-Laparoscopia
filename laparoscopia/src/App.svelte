01dd63a14bc783a3e066648da4483795deef7ca3
davix1997 <davixville@gmail.com>
2025-04-22 11:01:20 +0200

    funcion test

--- laparoscopia/src/App.svelte
+++ laparoscopia/src/App.svelte
@@ -4,22 +4,27 @@
 	import Instruments from "./components/Instruments.svelte";
 	import CameraControls from "./components/CameraControls.svelte";
 
-	let numInstruments = 3;
+	let numInstruments = 2;
 
 	const positions = ["top-right", "top-left", "bottom-left", "bottom-right"];
 
-	let depths = Array(numInstruments).fill(0);
+	let depths = Array(numInstruments).fill(50);
 	let pointsArray = Array(numInstruments).fill("0,0 -20,-30 20,-30");
 	let lnLeftArray = Array(numInstruments).fill("rotate(-30)");
 	let lnRightArray = Array(numInstruments).fill("rotate(30)");
 
-    lnLeftArray[1] = "rotate(-45)";
-    lnRightArray[1] = "rotate(45)";
-    pointsArray[1] = "0,0 -35,-30 35,-30";
+	
+    lnLeftArray[1] = "rotate(-25)";
+    lnRightArray[1] = "rotate(25)";
+    pointsArray[1] = "0,0 -15,-30 15,-30";
 
-    function prueba() {
-        let posicion = parseInt(prompt("Posicion"));
-        return positions[posicion];
+    function test() {
+        let position = parseInt(prompt("Posicion"));
+		if(position < 0 || position > 3 || isNaN(position)){
+			return test();
+		}else{
+			return positions[position];
+		}
     }
 </script>
 <style>
@@ -32,7 +37,7 @@
 
 {#each Array(numInstruments) as _, i}
 	<Instruments
-		position={prueba()}
+		position={positions[i]}
 		number={i + 1}
 		bind:depth={depths[i]}
 		points={pointsArray[i]}
